1e878fa2cfcaf02b0d1e038f2201000ba8caaffc
sunskwon <sunskwon@gmail.com>
2024-02-23 11:04:08 +0900

    develop version 0.0 BookDAO & book-query.xml

--- src/main/java/com/ohgiraffers/test/model/dao/BookDAO.java
+++ src/main/java/com/ohgiraffers/test/model/dao/BookDAO.java
@@ -1,4 +1,18 @@
 package com.ohgiraffers.test.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BookDAO {
+
+    private Properties prop = new Properties();
+
+    public BookDAO() {
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/test/mapper/book-query.xml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
