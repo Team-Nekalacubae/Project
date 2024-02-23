966a98801b1496b13aed8b7f9f160f3dcfa5574b
sunskwon <sunskwon@gmail.com>
2024-02-23 11:07:34 +0900

    develop version 0.0 MemberDAO & member-query.xml

--- src/main/java/com/ohgiraffers/test/model/dao/MemberDAO.java
+++ src/main/java/com/ohgiraffers/test/model/dao/MemberDAO.java
@@ -1,4 +1,18 @@
 package com.ohgiraffers.test.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class MemberDAO {
+
+    private Properties prop = new Properties();
+
+    public MemberDAO() {
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/test/mapper/member-query.xml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
