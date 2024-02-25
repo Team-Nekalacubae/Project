b8d367ee4fd81fe0ef904cc73b97c96770557a11
KANGMINSEO0 <juhee0629032@gmail.com>
2024-02-25 16:32:25 +0900

    feature-approveMember initial commit

--- src/main/java/com/ohgiraffers/test/run/Application.java
+++ src/main/java/com/ohgiraffers/test/run/Application.java
@@ -7,7 +7,6 @@ public class Application {
     public static void main(String[] args) {
 
         Menu menu = new Menu();
-//        menu.start();
-        menu.managerMenu();
+        menu.start();
     }
 }
